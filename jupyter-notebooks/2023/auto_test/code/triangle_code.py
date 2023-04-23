ea8a7e1efbbdfe479255ca0847d510eaedec0b5e
Hadron DaVinci <hd>
2023-04-23 01:55:43 -0400

    feat: added new notebook

---
+++ jupyter-notebooks/2023/auto_test/code/triangle_code.py
@@ -0,0 +1,15 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2023 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
+
+
+def triangle(x: int, y: int, z: int) -> str:
+    if x == y == z:
+        return "Equilateral triangle"
+    elif x == y or y == z or x == z:
+        return "Isosceles triangle"
+    else:
+        return "Scalene triangle"
